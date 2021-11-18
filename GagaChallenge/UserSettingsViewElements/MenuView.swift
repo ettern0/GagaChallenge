11c0d43cb1eeac291fce9946dfa060f5c0a50c2a
Elio Fortunato <eliowmb@gmail.com>
2021-11-18 13:27:48 +0100

    Added stuff in AdditionView

--- GagaChallenge/UserSettingsViewElements/MenuView.swift
+++ GagaChallenge/UserSettingsViewElements/MenuView.swift
@@ -76,8 +76,10 @@ struct MenuView: View {
                     Spacer(minLength: 40)
 
                     HStack{
-                        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-
+                        Button(action: {
+                            self.showGame = true
+                            self.currentGameView = AnyView(AdditionGameView())
+                        }) {
                             Image(systemName:"plus")
 
                                 .resizable()
